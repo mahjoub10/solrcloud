73b976a421806a817cc4deb9a8c01e5e7c3e48a7
Mahjoub MARZOUGUI <mmarzougui@Mahjoubs-MacBook-Pro.local>
2017-01-16 01:29:44 +0100

    update solr start shell

--- startSolr.sh
+++ startSolr.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+#ADD  ./bin/solr  /usr/bin/local
+ bin/solr zk upconfig -n flashConfig -d  flash/solr/flash/conf/    -z zoo1:2181
+
 bin/solr start -c -s server/solr -p 8983 -z localhost:2181,localhost:2182,localhost:2183 -noprompt
 
 bin/solr start -c -s server/solr2 -p 8984 -z localhost:2181,localhost:2182,localhost:2183 -noprompt
